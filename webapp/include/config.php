294331760a86ea013c4ef261b67870b65d348100
khahliso-kgodumo <khahlisokgodumo@gmail.com>
2025-05-16 03:35:25 +0200

    Add more functions to config.php

--- webapp/include/config.php
+++ webapp/include/config.php
@@ -10,14 +10,27 @@ class Database{
    private const DB_NAME = '';
 
    private function __construct(){
-       $this->connection = new mysqli(
-           self::DB_HOST, self::DB_USER, self::DB_PASS, self::DB_NAME
-       );
-       if($this->connection->connect_error){
-           throw new Exception("Database connection failed: " . $this->connection->connect_error);
-       }
+        $this->connection = new mysqli(
+            self::DB_HOST, self::DB_USER, self::DB_PASS, self::DB_NAME
+        );
+        if($this->connection->connect_error){
+            throw new Exception("Database connection failed: " . $this->connection->connect_error);
+        }
 
-       $this->connection->set_charset("utf8mb4");
+        $this->connection->set_charset("utf8mb4");
    }
+
+   public static getInstance(){
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+   }
+
+   public function getConnection(){
+	return $this->connection;
+   }
+   public function __clone(){}
+   public function __wakeup(){}
 }
 ?>
