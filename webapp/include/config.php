69f75a6339cc6c2e93dd43dcb7b2f56ab86a2fc7
khahliso-kgodumo <khahlisokgodumo@gmail.com>
2025-05-16 03:21:44 +0200

    Add config.php in app/include

---
+++ webapp/include/config.php
@@ -0,0 +1,23 @@
+<?php
+class Database{
+   private static $instance = null;
+   private $connection;
+
+   // db config
+   private const DB_HOST = '';
+   private const DB_USER = '';
+   private const DB_PASS = '';
+   private const DB_NAME = '';
+
+   private function __construct(){
+       $this->connection = new mysqli(
+           self::DB_HOST, self::DB_USER, self::DB_PASS, self::DB_NAME
+       );
+       if($this->connection->connect_error){
+           throw new Exception("Database connection failed: " . $this->connection->connect_error);
+       }
+
+       $this->connection->set_charset("utf8mb4");
+   }
+}
+?>
