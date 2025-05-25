ca1818043c5c7af75ec433ed11083bb967ced950
khahliso-kgodumo <102834265+khahliso-kgodumo@users.noreply.github.com>
2025-05-25 13:33:51 +0200

    This renders two line charts using mock data

---
+++ webapp/js/chart.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const ctx = document.getElementById('priceChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: ['May 5', 'May 10', 'May 15', 'May 20', 'May 25'],
+        datasets: [{
+            label: 'Average Price (per R1,000)',
+            data: [4, 4.3, 4.2, 4.1, 4.2],
+            borderColor: '#0d6efd',
+            fill: false,
+            tension: 0.3
+        }]
+        },
+        options: {
+        responsive: true,
+        scales: {
+            y: { beginAtZero: false }
+        }
+        }
+    });
+
+    const ctxWishlist = document.getElementById('wishlistChart').getContext('2d');
+    new Chart(ctxWishlist, {
+        type: 'line',
+        data: {
+        labels: ['May 5', 'May 10', 'May 15', 'May 20', 'May 25'],
+        datasets: [{
+            label: 'Average Saves (per 1,000)',
+            data: [0.5, 0.3, 0.32, 0.45, 0.65],
+            borderColor: '#0d6efd',
+            fill: false,
+            tension: 0.3
+        }]
+        },
+        options: {
+        responsive: true,
+        scales: {
+            y: { beginAtZero: false }
+        }
+        }
+    });
+;})
